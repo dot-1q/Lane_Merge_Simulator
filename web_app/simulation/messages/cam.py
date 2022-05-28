412bbc5b9d91eb135aeeb7fe7e384c69132a3ae7
dot-1q <tiagobarros99@ua.pt>
2022-05-28 20:09:18 +0100

    Improved messages Classes

--- web_app/simulation/messages/cam.py
+++ web_app/simulation/messages/cam.py
@@ -1,8 +1,45 @@
+from typing import Any, TypeVar, Type, cast
+
+
+T = TypeVar("T")
+
+
+def from_bool(x: Any) -> bool:
+    assert isinstance(x, bool)
+    return x
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
 class PublicTransportContainer:
     embarkation_status: bool
 
     def __init__(self, embarkation_status: bool) -> None:
         self.embarkation_status = embarkation_status
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'PublicTransportContainer':
+        assert isinstance(obj, dict)
+        embarkation_status = from_bool(obj.get("embarkationStatus"))
+        return PublicTransportContainer(embarkation_status)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["embarkationStatus"] = from_bool(self.embarkation_status)
+        return result
 
 
 class SpecialVehicle:
@@ -10,6 +47,17 @@ class SpecialVehicle:
 
     def __init__(self, public_transport_container: PublicTransportContainer) -> None:
         self.public_transport_container = public_transport_container
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'SpecialVehicle':
+        assert isinstance(obj, dict)
+        public_transport_container = PublicTransportContainer.from_dict(obj.get("publicTransportContainer"))
+        return SpecialVehicle(public_transport_container)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["publicTransportContainer"] = to_class(PublicTransportContainer, self.public_transport_container)
+        return result
 
 
 class CAM:
@@ -41,36 +89,7 @@ class CAM:
     width: int
     yaw_rate: int
 
-    def __init__(
-        self,
-        acc_engaged: bool,
-        acceleration: int,
-        altitude: int,
-        altitude_conf: int,
-        brake_pedal: bool,
-        collision_warning: bool,
-        cruise_control: bool,
-        curvature: int,
-        drive_direction: str,
-        emergency_brake: bool,
-        gas_pedal: bool,
-        heading: int,
-        heading_conf: int,
-        latitude: int,
-        length: int,
-        longitude: int,
-        semi_major_conf: int,
-        semi_major_orient: int,
-        semi_minor_conf: int,
-        special_vehicle: SpecialVehicle,
-        speed: int,
-        speed_conf: int,
-        speed_limiter: bool,
-        station_id: int,
-        station_type: int,
-        width: int,
-        yaw_rate: int,
-    ) -> None:
+    def __init__(self, acc_engaged: bool, acceleration: int, altitude: int, altitude_conf: int, brake_pedal: bool, collision_warning: bool, cruise_control: bool, curvature: int, drive_direction: str, emergency_brake: bool, gas_pedal: bool, heading: int, heading_conf: int, latitude: int, length: int, longitude: int, semi_major_conf: int, semi_major_orient: int, semi_minor_conf: int, special_vehicle: SpecialVehicle, speed: int, speed_conf: int, speed_limiter: bool, station_id: int, station_type: int, width: int, yaw_rate: int) -> None:
         self.acc_engaged = acc_engaged
         self.acceleration = acceleration
         self.altitude = altitude
@@ -98,3 +117,70 @@ class CAM:
         self.station_type = station_type
         self.width = width
         self.yaw_rate = yaw_rate
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'CAM':
+        assert isinstance(obj, dict)
+        acc_engaged = from_bool(obj.get("accEngaged"))
+        acceleration = from_int(obj.get("acceleration"))
+        altitude = from_int(obj.get("altitude"))
+        altitude_conf = from_int(obj.get("altitudeConf"))
+        brake_pedal = from_bool(obj.get("brakePedal"))
+        collision_warning = from_bool(obj.get("collisionWarning"))
+        cruise_control = from_bool(obj.get("cruiseControl"))
+        curvature = from_int(obj.get("curvature"))
+        drive_direction = from_str(obj.get("driveDirection"))
+        emergency_brake = from_bool(obj.get("emergencyBrake"))
+        gas_pedal = from_bool(obj.get("gasPedal"))
+        heading = from_int(obj.get("heading"))
+        heading_conf = from_int(obj.get("headingConf"))
+        latitude = from_int(obj.get("latitude"))
+        length = from_int(obj.get("length"))
+        longitude = from_int(obj.get("longitude"))
+        semi_major_conf = from_int(obj.get("semiMajorConf"))
+        semi_major_orient = from_int(obj.get("semiMajorOrient"))
+        semi_minor_conf = from_int(obj.get("semiMinorConf"))
+        special_vehicle = SpecialVehicle.from_dict(obj.get("specialVehicle"))
+        speed = from_int(obj.get("speed"))
+        speed_conf = from_int(obj.get("speedConf"))
+        speed_limiter = from_bool(obj.get("speedLimiter"))
+        station_id = from_int(obj.get("stationID"))
+        station_type = from_int(obj.get("stationType"))
+        width = from_int(obj.get("width"))
+        yaw_rate = from_int(obj.get("yawRate"))
+        return CAM(acc_engaged, acceleration, altitude, altitude_conf, brake_pedal, collision_warning, cruise_control, curvature, drive_direction, emergency_brake, gas_pedal, heading, heading_conf, latitude, length, longitude, semi_major_conf, semi_major_orient, semi_minor_conf, special_vehicle, speed, speed_conf, speed_limiter, station_id, station_type, width, yaw_rate)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["accEngaged"] = from_bool(self.acc_engaged)
+        result["acceleration"] = from_int(self.acceleration)
+        result["altitude"] = from_int(self.altitude)
+        result["altitudeConf"] = from_int(self.altitude_conf)
+        result["brakePedal"] = from_bool(self.brake_pedal)
+        result["collisionWarning"] = from_bool(self.collision_warning)
+        result["cruiseControl"] = from_bool(self.cruise_control)
+        result["curvature"] = from_int(self.curvature)
+        result["driveDirection"] = from_str(self.drive_direction)
+        result["emergencyBrake"] = from_bool(self.emergency_brake)
+        result["gasPedal"] = from_bool(self.gas_pedal)
+        result["heading"] = from_int(self.heading)
+        result["headingConf"] = from_int(self.heading_conf)
+        result["latitude"] = from_int(self.latitude)
+        result["length"] = from_int(self.length)
+        result["longitude"] = from_int(self.longitude)
+        result["semiMajorConf"] = from_int(self.semi_major_conf)
+        result["semiMajorOrient"] = from_int(self.semi_major_orient)
+        result["semiMinorConf"] = from_int(self.semi_minor_conf)
+        result["specialVehicle"] = to_class(SpecialVehicle, self.special_vehicle)
+        result["speed"] = from_int(self.speed)
+        result["speedConf"] = from_int(self.speed_conf)
+        result["speedLimiter"] = from_bool(self.speed_limiter)
+        result["stationID"] = from_int(self.station_id)
+        result["stationType"] = from_int(self.station_type)
+        result["width"] = from_int(self.width)
+        result["yawRate"] = from_int(self.yaw_rate)
+        return result
+
+
+def cam_from_dict(s: Any) -> CAM:
+    return CAM.from_dict(s)
--- web_app/simulation/messages/denm.py
+++ web_app/simulation/messages/denm.py
@@ -1,19 +1,34 @@
-from enum import Enum
+# To use this code, make sure you
+#
+#     import json
+#
+# and then, to convert JSON from a string, do
+#
+#     result = denm_from_dict(json.loads(json_string))
+
+from typing import Any, TypeVar, Type, cast
+
+T = TypeVar("T")
 
 
-class CauseCode(Enum):
-    MergeEvent = 31
-    Breaking = 32
-    SpeedingUp = 33
-    MaintainingVelocity = 34
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
 
 
-class SubCauseCode(Enum):
-    MergeRequest = 31
-    StartMerge = 32
-    FinishedMerge = 33
-    MergeDenied = 34
-    NotInvolved = 35
+def from_float(x: Any) -> float:
+    assert isinstance(x, (float, int)) and not isinstance(x, bool)
+    return float(x)
+
+
+def to_float(x: Any) -> float:
+    assert isinstance(x, float)
+    return x
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
 
 
 class ActionID:
@@ -24,6 +39,19 @@ class ActionID:
         self.originating_station_id = originating_station_id
         self.sequence_number = sequence_number
 
+    @staticmethod
+    def from_dict(obj: Any) -> 'ActionID':
+        assert isinstance(obj, dict)
+        originating_station_id = from_int(obj.get("originatingStationID"))
+        sequence_number = from_int(obj.get("sequenceNumber"))
+        return ActionID(originating_station_id, sequence_number)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["originatingStationID"] = from_int(self.originating_station_id)
+        result["sequenceNumber"] = from_int(self.sequence_number)
+        return result
+
 
 class Altitude:
     altitude_value: int
@@ -33,21 +61,44 @@ class Altitude:
         self.altitude_value = altitude_value
         self.altitude_confidence = altitude_confidence
 
+    @staticmethod
+    def from_dict(obj: Any) -> 'Altitude':
+        assert isinstance(obj, dict)
+        altitude_value = from_int(obj.get("altitudeValue"))
+        altitude_confidence = from_int(obj.get("altitudeConfidence"))
+        return Altitude(altitude_value, altitude_confidence)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["altitudeValue"] = from_int(self.altitude_value)
+        result["altitudeConfidence"] = from_int(self.altitude_confidence)
+        return result
+
 
 class PositionConfidenceEllipse:
     semi_major_confidence: int
     semi_minor_confidence: int
     semi_major_orientation: int
 
-    def __init__(
-        self,
-        semi_major_confidence: int,
-        semi_minor_confidence: int,
-        semi_major_orientation: int,
-    ) -> None:
+    def __init__(self, semi_major_confidence: int, semi_minor_confidence: int, semi_major_orientation: int) -> None:
         self.semi_major_confidence = semi_major_confidence
         self.semi_minor_confidence = semi_minor_confidence
         self.semi_major_orientation = semi_major_orientation
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'PositionConfidenceEllipse':
+        assert isinstance(obj, dict)
+        semi_major_confidence = from_int(obj.get("semiMajorConfidence"))
+        semi_minor_confidence = from_int(obj.get("semiMinorConfidence"))
+        semi_major_orientation = from_int(obj.get("semiMajorOrientation"))
+        return PositionConfidenceEllipse(semi_major_confidence, semi_minor_confidence, semi_major_orientation)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["semiMajorConfidence"] = from_int(self.semi_major_confidence)
+        result["semiMinorConfidence"] = from_int(self.semi_minor_confidence)
+        result["semiMajorOrientation"] = from_int(self.semi_major_orientation)
+        return result
 
 
 class EventPosition:
@@ -56,17 +107,28 @@ class EventPosition:
     position_confidence_ellipse: PositionConfidenceEllipse
     altitude: Altitude
 
-    def __init__(
-        self,
-        latitude: float,
-        longitude: float,
-        position_confidence_ellipse: PositionConfidenceEllipse,
-        altitude: Altitude,
-    ) -> None:
+    def __init__(self, latitude: float, longitude: float, position_confidence_ellipse: PositionConfidenceEllipse, altitude: Altitude) -> None:
         self.latitude = latitude
         self.longitude = longitude
         self.position_confidence_ellipse = position_confidence_ellipse
         self.altitude = altitude
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'EventPosition':
+        assert isinstance(obj, dict)
+        latitude = from_float(obj.get("latitude"))
+        longitude = from_float(obj.get("longitude"))
+        position_confidence_ellipse = PositionConfidenceEllipse.from_dict(obj.get("positionConfidenceEllipse"))
+        altitude = Altitude.from_dict(obj.get("altitude"))
+        return EventPosition(latitude, longitude, position_confidence_ellipse, altitude)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["latitude"] = to_float(self.latitude)
+        result["longitude"] = to_float(self.longitude)
+        result["positionConfidenceEllipse"] = to_class(PositionConfidenceEllipse, self.position_confidence_ellipse)
+        result["altitude"] = to_class(Altitude, self.altitude)
+        return result
 
 
 class Management:
@@ -77,21 +139,34 @@ class Management:
     validity_duration: int
     station_type: int
 
-    def __init__(
-        self,
-        action_id: ActionID,
-        detection_time: float,
-        reference_time: float,
-        event_position: EventPosition,
-        validity_duration: int,
-        station_type: int,
-    ) -> None:
+    def __init__(self, action_id: ActionID, detection_time: float, reference_time: float, event_position: EventPosition, validity_duration: int, station_type: int) -> None:
         self.action_id = action_id
         self.detection_time = detection_time
         self.reference_time = reference_time
         self.event_position = event_position
         self.validity_duration = validity_duration
         self.station_type = station_type
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Management':
+        assert isinstance(obj, dict)
+        action_id = ActionID.from_dict(obj.get("actionID"))
+        detection_time = from_float(obj.get("detectionTime"))
+        reference_time = from_float(obj.get("referenceTime"))
+        event_position = EventPosition.from_dict(obj.get("eventPosition"))
+        validity_duration = from_int(obj.get("validityDuration"))
+        station_type = from_int(obj.get("stationType"))
+        return Management(action_id, detection_time, reference_time, event_position, validity_duration, station_type)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["actionID"] = to_class(ActionID, self.action_id)
+        result["detectionTime"] = to_float(self.detection_time)
+        result["referenceTime"] = to_float(self.reference_time)
+        result["eventPosition"] = to_class(EventPosition, self.event_position)
+        result["validityDuration"] = from_int(self.validity_duration)
+        result["stationType"] = from_int(self.station_type)
+        return result
 
 
 class EventType:
@@ -102,6 +177,19 @@ class EventType:
         self.cause_code = cause_code
         self.sub_cause_code = sub_cause_code
 
+    @staticmethod
+    def from_dict(obj: Any) -> 'EventType':
+        assert isinstance(obj, dict)
+        cause_code = from_int(obj.get("causeCode"))
+        sub_cause_code = from_int(obj.get("subCauseCode"))
+        return EventType(cause_code, sub_cause_code)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["causeCode"] = from_int(self.cause_code)
+        result["subCauseCode"] = from_int(self.sub_cause_code)
+        return result
+
 
 class Situation:
     information_quality: int
@@ -111,6 +199,19 @@ class Situation:
         self.information_quality = information_quality
         self.event_type = event_type
 
+    @staticmethod
+    def from_dict(obj: Any) -> 'Situation':
+        assert isinstance(obj, dict)
+        information_quality = from_int(obj.get("informationQuality"))
+        event_type = EventType.from_dict(obj.get("eventType"))
+        return Situation(information_quality, event_type)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["informationQuality"] = from_int(self.information_quality)
+        result["eventType"] = to_class(EventType, self.event_type)
+        return result
+
 
 class DENM:
     management: Management
@@ -119,3 +220,24 @@ class DENM:
     def __init__(self, management: Management, situation: Situation) -> None:
         self.management = management
         self.situation = situation
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'DENM':
+        assert isinstance(obj, dict)
+        management = Management.from_dict(obj.get("management"))
+        situation = Situation.from_dict(obj.get("situation"))
+        return DENM(management, situation)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["management"] = to_class(Management, self.management)
+        result["situation"] = to_class(Situation, self.situation)
+        return result
+
+
+def denm_from_dict(s: Any) -> DENM:
+    return DENM.from_dict(s)
+
+
+def denm_to_dict(x: DENM) -> Any:
+    return to_class(DENM, x)
